5788d93e398efc5f1b74ddf9bd94d7dce17059c0
blanga021 <blanga021@student.wethinkcode.co.za>
2022-08-27 11:07:43 +0200

    Added website settings

--- medlyft/apps.py
+++ medlyft/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MedilyftConfig(AppConfig):
+class MedlyftConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medilyft'
+    name = 'medlyft'
--- synigency/settings.py
+++ synigency/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'medilyft.apps.MedilyftConfig',
+    'medlyft.apps.MedlyftConfig',
 ]
 
 MIDDLEWARE = [
