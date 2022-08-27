8e82a0f158e94b626ae6832ffdb316d132947acd
blanga021 <blanga021@student.wethinkcode.co.za>
2022-08-27 16:12:01 +0200

    Can save into database

--- medlyft/views.py
+++ medlyft/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Clinic
 
-
-# Create your views here.
 
 def index(request):
-    template = loader.get_template("Registration.html")
-    return HttpResponse(template.render())
+   template = loader.get_template("Registration.html")
+   return HttpResponse(template.render({}, request))
+
+
+def registration(request):
+   name = request.POST["Name"]
+   province = request.POST["Province"]
+   region = request.POST["Region"]
+   ward = request.POST["Ward"]
+   phone = request.POST["Phone"]
+   password = request.POST["Password"]
+
+   clinic = Clinic(
+      clinic_name=name,
+      province=province,
+      region=region,
+      tel_number=phone,
+      password=password,
+      ward_number=ward)
+
+   clinic.save()
+
+   return HttpResponseRedirect(reverse('index'))
+
+
+def redirect(request):
+   clinic = Clinic.objects.all()[0]
+   template = loader.get_template("redirect.html")
+   content = {
+      'clinic': clinic,
+   }
+
+   return HttpResponse(template.render(content, request))
+
