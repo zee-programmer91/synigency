f452bd5f9edd067bcc5b8487c12e506cac1735e1
xmosuma021 <xmosuma021@student.wethinkcode.co.za>
2022-08-27 12:28:48 +0200

    registration Models

--- medlyft/models.py
+++ medlyft/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Clinic(models.Model):
+    clinic_name = models.CharField(max_length=255)
+    province = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    ward_number = models.IntegerField()
+    tel_number= models.CharField(max_length=10)
+    password = models.CharField(max_length=255)
